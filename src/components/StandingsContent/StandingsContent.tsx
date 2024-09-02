231a8b3ed5ec786e729f4b05adc9c8de313e60b8
Ng, Ben <benjamin.b.ng@accenture.com>
2024-09-01 21:27:15 -0400

    standings work now

--- src/components/StandingsContent/StandingsContent.tsx
+++ src/components/StandingsContent/StandingsContent.tsx
@@ -3,12 +3,17 @@ import { PairsTable } from "../PairsTable/PairsTable";
 import { useEffect, useState } from "react";
 
 export function StandingsContent({ pairs }: StandingsContentProps) {
-    const [sortedPairs, setSortedPairs] = useState([...pairs]);
+    const [sortedPairs, setSortedPairs] = useState(pairs);
 
     useEffect(() => {
         const sp = sortedPairs.sort((a, b) => b.getWins() - a.getWins() || b.getPointDiff() - a.getPointDiff());
         setSortedPairs(sp);
     }, [])
+
+    useEffect(() => {
+        const sp = sortedPairs.sort((a, b) => b.getWins() - a.getWins() || b.getPointDiff() - a.getPointDiff());
+        setSortedPairs(sp);
+    }, [pairs])
 
     return (
         <>
